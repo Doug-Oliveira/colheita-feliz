744e5bf0dd361008b429607d4439779448cb1bc6
Doug-Oliveira <douglasdc778@gmail.com>
2021-11-28 02:09:44 -0300

    fix checkbox button

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -6,8 +6,8 @@ import {styles} from './styles';
 import { Checkbox, Colors, Modal, Portal, Provider, Button, TextInput } from "react-native-paper";
 import { Component } from "react"; 
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import FlashMessage, { showMessage, hideMessage } from "react-native-flash-message";
-
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 
@@ -17,37 +17,49 @@ export default class Home extends Component{
 
   constructor(props) {
     super(props);
-    this.second_state = {
-      checked: false
-    }
+    this.getData();
+   
+    this.funcao()
+   
     this.state = {
       visible: false,
-      checked: false,
+      checked: 'IA',
+      
       text_input: "",
         teste: {plantingSituation:{          
           "namePlantation": "---",
           "typeOfIrrigation": "----",
           "PlantingSituation_typeOfIrrigation": "---",
-          "PlantingSituation_moisture": "",
+          "PlantingSituation_moisture": "00.00",
           "PlantingSituation_IrrigationDate": "---"
-        }
+        },
       }
     };
+
+    this.second_state = {
+      checked: this.state.teste.plantingSituation.PlantingSituation_typeOfIrrigation //aqui2
+    }
   }
 
   
   //Refresh Irrigation Request
   async sendRequest() :Promise <JSON> {
     
-    const response = await fetch('http://http://192.168.0.45:3213/plantation/get-humidity', {
-      method: 'GET'
-
+    const response = await fetch('http://localhost:3213/platation/get-plantation', {
+      method: 'POST',
+      headers: {
+        "Content-type": "application/json",
+        "id": "e2a06b96-5c1b-486c-8e1e-f500d537c0d6"
+      },
+      body: JSON.stringify({
+      })
     })
       .then(async res => {
         return res.json()
       })
       .then(data => {
         this.setState({teste: data})
+        this.setState({checked: this.state.teste.plantingSituation.typeOfIrrigation})
         console.log(this.state.teste.plantingSituation.namePlantation)
         
       })
@@ -59,7 +71,7 @@ export default class Home extends Component{
   //request that sends information to enable automatic irrigation
   async sendIrrigationCheckMark(opt: String) :Promise <JSON> {
     
-    const response = await fetch('http://192.168.0.45:3213/platation/toggle-typeIrrigation', {
+    const response = await fetch('http://localhost:3213/platation/toggle-typeIrrigation', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -113,8 +125,26 @@ export default class Home extends Component{
     async changeChebox(opt: boolean) :Promise <JSON>{
       opt = opt ? "Manual" : "IA"
       await this.sendIrrigationCheckMark(opt)
-      console.log(opt)
-    }  
+      //console.log(opt)
+      
+      try{
+        await AsyncStorage.setItem('checked', this.state.checked)
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+    getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('checked')
+
+        if (value !== null){
+          this.setState({checked: value})
+        }
+      }catch(e){
+        console.log(e)
+      }
+    }
 
   render(){
     return(
@@ -125,7 +155,7 @@ export default class Home extends Component{
                 Sr. Jorge
               </Text>
               <View style={styles.plantation_title}>
-                <Text style={styles.plantation_title_content}>Umidade em: {this.state.teste.plantingSituation.PlantingSituation_moisture}%</Text>
+                <Text style={styles.plantation_title_content}>Umidade em: {parseFloat(this.state.teste.plantingSituation.PlantingSituation_moisture).toFixed(0)}%</Text>
                 <MaterialCommunityIcons 
                   style={styles.icon}
                   name="refresh-circle" 
@@ -139,7 +169,7 @@ export default class Home extends Component{
                 resizeMode="stretch"/>
                 <Text style={styles.plantation_information}>{this.state.teste.plantingSituation.namePlantation}</Text>
                 <Text style={styles.last_irrigation}>Última irrigação: <Text style={styles.font_styled}>{this.state.teste.plantingSituation.PlantingSituation_IrrigationDate}</Text></Text>
-                <Text style={styles.last_irrigation}>Tipo: <Text style={styles.font_styled}>{this.state.teste.plantingSituation.typeOfIrrigation}</Text></Text>
+                <Text style={styles.last_irrigation}>Tipo: <Text style={styles.font_styled}>{this.state.teste.plantingSituation.PlantingSituation_typeOfIrrigation}</Text></Text>
                 <View style={styles.plantation_card_toggle}>
                   <Text style={styles.ia_text}>Ativar Automação
                   <Checkbox
@@ -147,11 +177,22 @@ export default class Home extends Component{
                     style={styles.checked}
                     color={Colors.green500}
                     uncheckedColor="#99A799"
-                    status={this.state.checked ? 'checked' : 'unchecked'}
+                    status={this.state.checked == 'IA' ? 'checked' : 'unchecked'}
                     onPress={() => { 
-                      this.setState({this.state.checked = !this.state.checked})
-                        this.setState({checked: !this.state.checked})
-                        this.changeChebox(this.state.checked)
+
+                      console.log(this.state.checked)
+                      if(this.state.teste.plantingSituation.typeOfIrrigation == 'Manual') this.setState({checked: 'IA'})
+                      else this.setState({checked: 'Manual'})
+
+
+                      //this.setState({this.state.checked = !this.state.checked})
+                       //aqui
+
+                       this.state.checked == 'IA' ? this.changeChebox(true) :this.changeChebox(false) 
+
+                       this.funcao();
+                        
+                        //this.onPressCheckbox()
                       }
                     }
                   />
@@ -172,7 +213,6 @@ export default class Home extends Component{
                       onChangeText={(text)=> {
                         this.setState({text_input: text})
                         const aux_params = this.state.text_input / 10
-                        console.log(aux_params)
                       }}
                       mode="flat"
                       label="Nivel de umidade"
